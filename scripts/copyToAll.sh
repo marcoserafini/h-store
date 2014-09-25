ba10073c401f3f64676d4c2e7a57fe35d9ca92c2
marcoserafini <marco.serafini@gmail.com>
2014-09-25 13:37:55 +0300

    tried to run estore comparison with accordion

--- scripts/copyToAll.sh
+++ scripts/copyToAll.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# copies a specific file (given as argument) to all other servers
 
 for s in da01 da02 da04 da05 da06 da07 da08 da09 da10 da11 da12 da14 da15
 do scp $1 $s:/localdisk/mserafini/h-store/$1
