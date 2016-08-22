c4f35e00f4bc52ccbab60fe3a58f07cb48bd64cc
Marco Serafini <marco.serafini@gmail.com>
2016-08-22 05:02:49 -0400

    fixed script

--- scripts/save_results.sh
+++ scripts/save_results.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 mkdir $1
+cp plan.json $1
 cp plan_out.json $1
 cp -r obj/logs $1-logs
 cp results.csv $1
 cp out.log $1
+cp out-controller.log $1
+cp out-load.log $1
 cp hevent.log $1
